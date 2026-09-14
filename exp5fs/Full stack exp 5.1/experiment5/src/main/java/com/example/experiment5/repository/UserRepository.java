f5ad4ea10f1984618097d4ab13ae3dc72a7a8440
existential7 <srivasatavaaryan098@gmail.com>
2026-09-14 16:10:29 +0530

    Delete

--- exp5fs/Full stack exp 5.1/experiment5/src/main/java/com/example/experiment5/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.experiment5.repository;
-
-import com.example.experiment5.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- exp5fs/Full stack exp 5.2/experiment5/src/main/java/com/example/experiment5/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.experiment5.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
