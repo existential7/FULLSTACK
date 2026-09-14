d025a6d12bfc22ac99e1c24cd9d7a08e699385f7
existential7 <srivasatavaaryan098@gmail.com>
2026-09-14 16:09:24 +0530

    Delete exp1fs directory

--- exp1fs/1.1/code/platformConfig.js
+++
@@ -1,5 +0,0 @@
-export const PLATFORM_LIMITS = {
-  Twitter: { maxChars: 280, mediaRequired: false, maxHashtags: 10 },
-  Instagram: { maxChars: 2200, mediaRequired: true, maxHashtags: 30 },
-  LinkedIn: { maxChars: 3000, mediaRequired: false, maxHashtags: 5 },
-};
